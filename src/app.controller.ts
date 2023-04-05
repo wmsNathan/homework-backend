59ea47b8ef04bd6c49b524223418e194ac706267
박근호 <parknathan@baggeunhoui-MacBookAir.local>
2023-04-05 15:45:13 +0900

    init

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/name/:n')
+  getTitle(@Param() params): string {
+    return 'Title is' + params.n;
+  }
+}
